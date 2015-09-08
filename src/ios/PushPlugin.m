f0f95afa94fea65ee92580890c5083fe28adab06
Evernym <aditya@evernym.us>
2015-09-08 15:52:27 +0530

    Revert NSUInteger type changes

--- src/ios/PushPlugin.m
+++ src/ios/PushPlugin.m
@@ -179,7 +179,7 @@
     [results setValue:[[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"] forKey:@"appVersion"];
     
     // Check what Notifications the user has turned on.  We registered for all three, but they may have manually disabled some or all of them.
-    NSUInteger rntypes = [[[UIApplication sharedApplication] currentUserNotificationSettings] types];
+    NSUInteger rntypes = [[UIApplication sharedApplication] enabledRemoteNotificationTypes];
     
     // Set the defaults to disabled unless we find otherwise...
     NSString *pushBadge = @"disabled";
